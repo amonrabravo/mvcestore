00404e09d1ebfdd723afb9c514df3ec8af0d219a
Önay YALÇINER <onay.yalciner@hotmail.com>
2021-04-08 09:19:21 +0300

    scss

---
+++ MVCEStore/MVCEStoreWeb/Components/RayonMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcEStoreData;
+using System.Linq;
+
+namespace MVCEStoreWeb.Components
+{
+    public class RayonMenuViewComponent : ViewComponent
+    {
+        private readonly AppDbContext context;
+
+        public RayonMenuViewComponent(
+            AppDbContext context
+            )
+        {
+            this.context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = context.Rayons.Where(p => p.Enabled && p.Categories.Any()).OrderBy(p => p.SortOrder).ToList();
+            return View(model);
+        }
+    }
+}
